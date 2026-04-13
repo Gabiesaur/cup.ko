0b4f9faa48ad25601b89995a340a95c96713023b
Gabiesaur <gabrielleoncio2005@gmail.com>
2026-04-13 14:59:30 +0800

    ADDED FONT

--- cup_ko/client/src/pages/LandingPage.tsx
+++ cup_ko/client/src/pages/LandingPage.tsx
@@ -1,7 +1,14 @@
+import landing_bg from '../assets/landing_bg.png';
+
 const LandingPage = () => {
     return (
-        <main>
-            <h1>Welcome</h1>
+        <main
+            className='min-h-screen w-full bg-cover bg-center bg-no-repeat'
+            style={{ backgroundImage: `url(${landing_bg})`}}
+        >
+            <h1 style={{ fontFamily: "'Opun Mai Bold Italic'" }}>
+                Chewy with a Tropical Soul.
+            </h1>
         </main>
     )
 }
