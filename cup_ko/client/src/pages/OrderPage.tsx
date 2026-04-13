68c08bf82a8fbb6aad97baae034b596f3cf8a192
Martin <martinmanalo5@gmail.com>
2026-04-13 22:20:35 +0800

    global background

--- cup_ko/client/src/pages/OrderPage.tsx
+++ cup_ko/client/src/pages/OrderPage.tsx
@@ -11,12 +11,12 @@ type OrderPageProps = {
 function OrderPage({ items, onAddToCart, cartCount }: OrderPageProps) {
   return (
     <div className="flex flex-col items-center pb-16">
-      <div className="w-full max-w-7xl px-8 pt-10 flex items-center justify-center">
+      <div className="w-full max-w-7xl px-8 pt-16 flex items-center justify-center">
         <h1 className="font-opun text-pink-200 text-stroke-cake text-6xl">
           Chewy Cake Bites
         </h1>
       </div>
-      <div className="mt-16 px-8">
+      <div className="mt-16 p-16 bg-white border-2 border-black rounded-4xl">
         <div className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-4">
           {items.map((item) => (
             <Item
