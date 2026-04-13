f05fe6020dd940d91283c0fa90c4b5b5fa5fc905
Gabiesaur <gabrielleoncio2005@gmail.com>
2026-04-13 22:59:16 +0800

    changed to button

--- cup_ko/client/src/components/InfoBlock.tsx
+++ cup_ko/client/src/components/InfoBlock.tsx
@@ -3,16 +3,22 @@ import React from 'react';
 interface InfoBlockProps {
     title: string;
     description: string;
+    onClick?: () => void; // Added an optional onClick handler
 }
 
-const InfoBlock: React.FC<InfoBlockProps> = ({ title, description }) => {
+const InfoBlock: React.FC<InfoBlockProps> = ({ title, description, onClick }) => {
     return (
         <div className='w-full md:w-[500px]'>
-            <div className='h-16 md:h-20 bg-[#fcd990] rounded-full md:rounded-4xl flex justify-center items-center'>
-                <p className='text-[#e1a0aa] text-2xl md:text-5xl font-bold' style={{ fontFamily: "Opun Mai Bold Italic" }}>
+            {/* Changed from div to button, added w-full and hover effects */}
+            <button 
+                type="button"
+                onClick={onClick}
+                className='w-full h-16 md:h-20 bg-[#fcd990] rounded-full md:rounded-4xl flex justify-center items-center hover:opacity-90 transition-opacity cursor-pointer border-none'
+            >
+                <p className='text-[#e1a0aa] text-2xl md:text-5xl font-bold m-0' style={{ fontFamily: "Opun Mai Bold Italic" }}>
                     {title}
                 </p>
-            </div>
+            </button>
             <ul className='pl-6 md:pl-8 mt-3'>
                 <li className='text-[#FFFFFF] list-disc text-base md:text-xl'>
                     {description}
